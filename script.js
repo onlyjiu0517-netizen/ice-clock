3643cff5c476ebe74d80dc764db4c1c33a972b6a
onlyjiu0517-netizen <onlyjiu0517@gmail.com>
2026-07-14 20:33:57 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// =========================
+// ice-clock
+// Clock
+// =========================
+
+const hourElement = document.getElementById("hour");
+const minuteElement = document.getElementById("minute");
+
+function updateClock() {
+    const now = new Date();
+
+    const hour = String(now.getHours()).padStart(2, "0");
+    const minute = String(now.getMinutes()).padStart(2, "0");
+
+    hourElement.textContent = hour;
+    minuteElement.textContent = minute;
+}
+
+// 最初に表示
+updateClock();
+
+// 1秒ごとに更新
+setInterval(updateClock, 1000);
